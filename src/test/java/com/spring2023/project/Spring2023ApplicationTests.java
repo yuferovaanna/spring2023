8a1a6766a972f53525d1a9d4f189575d57fd34dd
Anna <yuferova.anna@yandex.ru>
2023-09-19 22:18:11 +0500

    1.1 Создание проекта Spring Boot

---
+++ src/test/java/com/spring2023/project/Spring2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring2023.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
